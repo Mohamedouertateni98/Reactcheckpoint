27afacef39b52e9718fe3e295cb2a7cf7f1d4f86
Mohamedouertateni98 <99283037+Mohamedouertateni98@users.noreply.github.com>
2022-08-23 17:39:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigi from './Navigi/Navigi';
+import Formu from './formu/Formu';
+import Footerend from './Footerend/Footerend'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './Container1/Cards'
+
+
+
+function App() {
+  return (
+    <>
+    <Navigi/>
+    <Cards/>
+    <Formu/>
+    <Footerend/>
+    
+    </>
+  );
+}
+
+export default App;
